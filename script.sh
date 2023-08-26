c3426f9f491428c9c56ed8ad6000f456b364c155
root <root@LAPTOP-OM49846M.localdomain>
2023-08-26 19:05:36 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo wget -P /var/www/html https://www.free-css.com/assets/files/free-css-templates/download/page284/pet-shop.zip
+sudo unzip -o /var/www/html/pet-shop.zip -d /var/www/html
+sudo cp -rf /var/www/html/pet-shop-website-template/* /var/www/html/
+sudo systemctl restart httpd.service
